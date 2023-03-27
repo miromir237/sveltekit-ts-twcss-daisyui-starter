2d7f0217271595afdd83779c40d86e18cab368af
Miroslav Pilat <miroslav.pilat@gmail.com>
2023-03-27 17:22:03 +0200

    first commit

---
+++ src/lib/config.js
@@ -0,0 +1,13 @@
+/**
+ * All of these values are used throughout the site – for example, 
+ * in the <meta> tags, in the footer, and in the RSS feed.
+ * 
+ * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
+ **/ 
+
+export const siteTitle = 'sveltekit-ts-twcss-daisyui-starter'
+export const siteDescription = 'SvelteKit-TS web starter templated with TailwindCSS and DaisyUI'
+export const siteURL = 'http://siteurl.com'
+export const siteLink = 'https://github.com/miromir237/sveltekit-ts-twcss-daisyui-starter'
+export const siteAuthor = 'Miroslav Pilat'
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div class="hero min-h-screen" style="background-image: url(https://placeimg.com/640/480/tech);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+        <p class="mb-5">Thanks for stopping by.</p>
+        <p class="mb-5">This is just a template. Startup building your webpage with 
+          <a href="https://tailwindcss.com/docs/installation" class="link">TailwindCSS</a> and <a href="https://daisyui.com/" class="link">DaisyUI</a>.
+        </p>
+      </div>
+    </div>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
